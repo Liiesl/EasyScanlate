415d90f62f7afd700905aaff64e424fde1d8cc36
Liiesl <gobloklo777@gmail.com>
2025-09-18 14:25:18 +0700

    Rewrite the app to PySide6 (#13)
    * find and replace import from pyqt5 to pyside6
    
    * fix missed pyqtsignal import. also  resolve qaction, pyqtslot,and qshortcut import.
    
    * fix find replace init error
    
    * fix double home window
    
    * Github Action for Nuitka Compilation  (#14)
    
    * use centralize app_icon
    
    * add pop up msg if don't have pyside6
    
    * load qsettings on entry preload
    
    * log home ui creation and show on splach screen, remove all time sleep
    
    * make pyqt5 error more robust
    
    * fix nuitka argument on github workflow
    
    * make pyqt5 import ro be nuitka compile safe
    
    * Merge branch 'test-deploy' of https://github.com/Liiesl/ManhwaOCR into pyside6-rewrite
    
    - try to solve whatever the problem on easyocr,bidi import chain
    - fix dir path in upload artifact
    - test using python 3.12 and mingw64
    - separately install torch
    
    * move app icon.ico to assets
    
    * improve load time of import package
    
    * fix import issues on compiled by moving and include easyocr models to root.
    
    * fix up the include data dir
    
    * remove dot from easyocr models
    
    * update todo
    
    * delete ico from launcher

--- app/utils/project_processing.py
+++ app/utils/project_processing.py
@@ -4,9 +4,9 @@ import json
 import tempfile
 import re
 from shutil import copyfile, rmtree
-from app.ui.dialogs import NewProjectDialog, ImportWFWFDialog
-from PyQt5.QtWidgets import QMessageBox, QFileDialog, QDialog, QApplication
-from PyQt5.QtCore import QDateTime, QDir, Qt
+from app.ui.dialogs.project_dialog import NewProjectDialog, ImportWFWFDialog
+from PySide6.QtWidgets import QMessageBox, QFileDialog, QDialog, QApplication
+from PySide6.QtCore import QDateTime, QDir, Qt
 
 def new_project(self):
     dialog = NewProjectDialog(self)
@@ -126,7 +126,7 @@ def launch_project(self, mmtl_path):
         self.launch_main_app(mmtl_path)
     elif hasattr(self, 'main_window'):
         # Called from MenuBar class
-        from main import LoadingDialog, ProjectLoaderThread
+        from app.ui.window.main_window import LoadingDialog, ProjectLoaderThread
         
         # Show loading dialog
         loading_dialog = LoadingDialog(self)
